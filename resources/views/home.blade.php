1b66a866225cef37932bae07fd2a30ac5ff0baee
Ximena <ximecediaz@outlook.com>
2020-12-12 14:42:08 -0600

    Agregar imagenes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,26 +3,31 @@
 @section('contenido')
 <div class="imagen d-flex align-items-center">
   <div class="col col-12">
-  <div class="row justify-content-center mb-5">
-      <div>
-        <h1>Lorem ipsum dolor sit amet.</h1>
-      </div>
-  </div>
+    <div class="row justify-content-center mb-5">
+        <div>
+          <h1>Lorem ipsum dolor sit amet.</h1>
+        </div>
+    </div>
 
-  <div class="row justify-content-center">
-    <div class="input-group md-form form-sm form-2 pl-0" style="max-width: 80%;">
-      <input class="form-control my-0 py-1 red-border" type="text" placeholder="Search" aria-label="Search">
-      <div class="input-group-append">
-        <span class="input-group-text bg-danger lighten-3" id="basic-text1">
-          <i class="fas fa-search"></i>
-        </span>
+    <div class="row justify-content-center">
+      <div class="input-group md-form form-sm form-2 pl-0" style="max-width: 80%;">
+        <input class="form-control my-0 py-1 red-border" type="text" placeholder="Search" aria-label="Search">
+        <div class="input-group-append">
+          <span class="input-group-text bg-danger lighten-3" id="basic-text1">
+            <i class="fas fa-search"></i>
+          </span>
+        </div>
       </div>
     </div>
-  </div>
 
-  <div class="row justify-content-center mt-4">
-    <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, illo!</span>
+    <div class="row justify-content-center mt-4">
+      <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, illo!</span>
+    </div>
   </div>
 </div>
+@auth
+<div class="mx-auto mt-5" style="width: 50%;">
+<a class="btn btn-primary btn-lg btn-block" href="/imagen">Agregar imágenes</a>
 </div>
+@endauth
 @endsection
--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -3,6 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
@@ -28,17 +32,27 @@
     </ul>
   </div>
   @auth
-    <div class="admin"><p>Administrador</p></div>
-    <div>
-      <a href="{{ route('logout') }}"
-        onclick="event.preventDefault();
-        document.getElementById('logout-form').submit();">
-        {{ __('Cerrar sesión') }}
+  <ul class="navbar-nav ml-auto mr-4">
+    <li class="nav-item"><i class="fas fa-user"></i></li>
+    <li class="nav-item dropdown">
+    
+      <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+        {{ Auth::user()->name }}
+      </a>
+
+      <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+        <a class="dropdown-item" href="{{ route('logout') }}"
+          onclick="event.preventDefault();
+            document.getElementById('logout-form').submit();">
+              {{ __('Cerrar sesión') }}
         </a>
-      <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-        @csrf
-      </form>
-    </div>
+
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+              @csrf
+            </form>
+      </div>
+    </li>
+  </ul>
   @endauth
   <button type="button" class="btn btn-light">Donar al autor</button>
 </nav>
